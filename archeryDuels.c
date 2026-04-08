48af96c15db692baafaa88623e44597210a855fa
gnosisphobia <chumimi9190@gmail.com>
2026-04-08 16:50:37 -0300

    Add files via upload

--- archeryDuels.c
+++ archeryDuels.c
@@ -2,24 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-void resultadoDisparo(int valorSorteio) {
+void rodada() {
   srand(time(0));
 
-  int numeroSorteio = (rand() % 6) + 1; //
+  int numeroSorteio = (rand() % 6) + 1;
 }
 int main() {
 
   int ptsJogador = 0, ptsComputador = 0;
   int flechasJogador = 3, flechasComputador = 3;
   int turno = 0; // jogador: 0, computador: 1
+  int escolhaDoJogador;
+  int numeroLoopJogador = 0;
+
   while (ptsJogador < 100 || ptsComputador < 100) {
-    if (turno == 0) {
-      printf("=== Turno do jogador ===");
+    while (turno == 0 || numeroLoopJogador != 3) {
+      printf("=== Turno do jogador ===\n");
+      printf("Escolha sua ação:\n1. Atirar\n2. Encerrar rodada");
+      scanf("%d", &escolhaDoJogador);
 
-    } else {
-      printf("=== Turno do computador ===");
+      if (escolhaDoJogador == 1) {
+        rodada();
+        numeroLoopJogador++;
+      } else {
+        numeroLoopJogador++;
+      }
     }
-  }
 
-  return 0;
+    return 0;
+  }
 }
